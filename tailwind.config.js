a8c7a035da34f3d0b0418ab229dc94e0b630dbd6
SebastienLaurentt <slaurent.26@gmail.com>
2023-07-15 15:50:51 +0200

    install Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    fontSize: {
+      sm: '0.75rem',
+      base: '0.875rem',
+      md: '1rem',
+      lg: '1.125rem',
+      xl: '1.5rem',
+      '2xl': '1.75rem',
+      '3xl': '2rem',
+      '4xl': '2.5rem',
+      '5xl': '3.5rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
