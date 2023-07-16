dde76d12e5ef7b9434e2cfb5d732186511aadab1
SebastienLaurentt <slaurent.26@gmail.com>
2023-07-16 18:18:47 +0200

    experiences page shape

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,11 @@
+import Experiences from "../Sections/Experiences";
 import Home from "../Sections/Home";
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Experiences/>
     </div>
   );
 }
---
+++ src/components/Sections/Experiences.tsx
@@ -0,0 +1,34 @@
+
+function Experiences () {
+    return (
+    <section className="bg-gradient-to-t from-slate-900 to-gray-700 min-h-screen p-8">
+        <h2 className="bg-gradient-to-r from-teal-700 to-cyan-400 bg-clip-text text-transparent text-center text-3xl">Experiences</h2>
+        <div className="flex flex-row justify-between mt-4">
+            <button className="bg-gradient-to-r from-teal-700 to-cyan-400 text-white flex flex-col p-2 rounded-lg ">
+                <span>PhD Student</span>
+                <span>2017 - 2021</span>
+            </button>
+            <button className="bg-gradient-to-r from-teal-700 to-cyan-400 text-white flex flex-col p-2 rounded-lg ">
+                <span>Post Doc</span>
+                <span>2021 - 2022</span>
+            </button>
+            <button className="bg-gradient-to-r from-teal-700 to-cyan-400 text-white flex flex-col p-2 rounded-lg ">
+                <span>Web Dev</span>
+                <span>2022 - 2023</span>
+            </button>
+        </div>
+        <section className="p-4">
+          <div className=" text-white w-full text-center mt-28 mb-8">
+            <span className="block text-3xl"> Welcome !  </span>
+            <span className=" block bg-gradient-to-r from-teal-700 to-cyan-400 bg-clip-text text-transparent text-4xl mb-4"> I'm Sébastien</span>
+            <p className="text-lg mb-4"> A cognitive researcher who wants to use brain knowledge to build an accessible web </p>
+            <button className="bg-gradient-to-r from-teal-700 to-cyan-400 text-md p-4 rounded-full">DOWNLOAD CV</button>
+          </div>
+          <img src="" alt="" className="" />
+        </section>
+
+
+    </section>
+    );
+  }
+export default Experiences;
