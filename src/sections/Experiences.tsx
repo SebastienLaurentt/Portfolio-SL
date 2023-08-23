f4c07f7cd7b74ff395b1bbaa91c95e3bcc8f4bb4
SebastienLaurentt <slaurent.26@gmail.com>
2023-08-23 21:26:19 +0200

    responsive for project section

--- src/sections/Experiences.tsx
+++ src/sections/Experiences.tsx
@@ -19,49 +19,56 @@ function Experiences() {
 
   if (activeButton === 'PhD Student') {
     sectionContent = (
-      <div className="">
-        <h3 className="mb-0">PhD Student</h3>
-        <h4 className="">Gustave Eiffel University</h4>
-        <p className="mb-2">
-        The aim of this thesis was to <strong className='text-emerald-500'>adapt</strong>  the Highway Code learning aid for deaf people to <strong className='text-emerald-500'>suit</strong> their <strong className='text-emerald-500'>cognitive profile</strong>.
-        </p>
-        <p className="mb-2">
-        Experiments were carried out to collect behavioral and physiological <strong className='text-emerald-500'>data</strong> using <strong className='text-emerald-500'>oculometry</strong>, as well as <strong className='text-emerald-500'>brain</strong> activity.
-        </p>
-
-
-        <img src={brainlogo} alt="An illustration of researchers doing brain research. (Copyright @ https://fr.freepik.com/vecteurs-libre/illustration-concept-chimie-du-cerveau_29654747.htm#&position=2&from_view=user)" className="mt-4 md:mt-8 h-40 md:h-80 mx-auto" />
-      </div>
+      <>
+        <div className='lg:w-1/2'>
+          <h3 className="mb-0">PhD Student</h3>
+          <h4 className="">Gustave Eiffel University</h4>
+          <p className="mb-2">
+          The aim of this thesis was to <strong className='text-emerald-500'>adapt</strong>  the Highway Code learning aid for deaf people to <strong className='text-emerald-500'>suit</strong> their <strong className='text-emerald-500'>cognitive profile</strong>.
+          </p>
+          <p className="mb-2">
+          Experiments were carried out to collect behavioral and physiological <strong className='text-emerald-500'>data</strong> using <strong className='text-emerald-500'>oculometry</strong>, as well as <strong className='text-emerald-500'>brain</strong> activity.
+          </p>
+        </div>
+        <div className= "lg:w-2/3 lg:flex lg:justify-end">
+          <img src={brainlogo} alt="An illustration of researchers doing brain research. (Copyright @ https://fr.freepik.com/vecteurs-libre/illustration-concept-chimie-du-cerveau_29654747.htm#&position=2&from_view=user)" className="mt-4 md:mt-8 h-40 md:h-80 mx-auto lg:mx-0  " />
+        </div>
+      </>
     );
   } else if (activeButton === 'Post Doc') {
     sectionContent = (
       <>
-        <h3 className="mb-0">Post Doc</h3>
-        <h4 className="">Gustave Eiffel University</h4>
-        <p className="mb-2">
-        This post-doctorate was linked to a <strong className='text-emerald-500'>European</strong> project to assess the <strong className='text-emerald-500'>acceptability</strong> of <strong className='text-emerald-500'>autonomous</strong> vehicles for young and elderly people.
-        </p>
-        <p className="mb-2">
-        A <strong className='text-emerald-500'>driving</strong> simulator was used to immerse participants in various <strong className='text-emerald-500'>scenarios</strong> to gather their <strong className='text-emerald-500'>impressions</strong>. 
-        </p>
-
-
-        
-        <img src={carlogo} alt="An illustration of an individual in a car without a steering wheel because it is an autonomous vehicle. (Copyright @ https://fr.freepik.com/vecteurs-libre/illustration-concept-voiture-autonome_19184615.htm#&position=1&from_view=user)" className="mt-4 md:mt-8 h-40 md:h-80 mx-auto" />
+        <div className='lg:w-1/2'>
+          <h3 className="mb-0">Post Doc</h3>
+          <h4 className="">Gustave Eiffel University</h4>
+          <p className="mb-2">
+          This post-doctorate was linked to a <strong className='text-emerald-500'>European</strong> project to assess the <strong className='text-emerald-500'>acceptability</strong> of <strong className='text-emerald-500'>autonomous</strong> vehicles for young and elderly people.
+          </p>
+          <p className="mb-2">
+          A <strong className='text-emerald-500'>driving</strong> simulator was used to immerse participants in various <strong className='text-emerald-500'>scenarios</strong> to gather their <strong className='text-emerald-500'>impressions</strong>. 
+          </p>
+        </div>
+        <div className= "lg:w-2/3 lg:flex lg:justify-end">
+          <img src={carlogo} alt="An illustration of an individual in a car without a steering wheel because it is an autonomous vehicle. (Copyright @ https://fr.freepik.com/vecteurs-libre/illustration-concept-voiture-autonome_19184615.htm#&position=1&from_view=user)" className="mt-4 md:mt-8 h-40 md:h-80 mx-auto lg:mx-0" />
+        </div>
       </>
     );
   } else if (activeButton === 'Web Dev') {
     sectionContent = (
       <>
-        <h3 className="mb-0">Web Dev</h3>
-        <h4 className="">O'Clock Company</h4>
-        <p className="mb-2">
-        The goal of this training was to provide the necessary <strong className='text-emerald-500'>foundations</strong> for website development, teaching programming languages such as <strong className='text-emerald-500'>JavaScript</strong> or <strong className='text-emerald-500'>PHP</strong>.
-        </p>
-        <p className="mb-2">
-        A final project (<i><strong className='text-emerald-500'>Codit</strong>, see below</i>) was carried out over a duration of <strong className='text-emerald-500'>1 month</strong> to consolidate the knowledge acquired during the previous month.
-        </p>
-        <img src={computerlogo} alt="An illustration of a laptop used for programming. (Copyright @ https://fr.freepik.com/vecteurs-libre/illustration-concept-codage-main_21864184.htm#&position=0&from_view=user) " className="mt-4 md:mt-12 h-40 md:h-80 mx-auto" />
+        <div className='lg:w-1/2'>
+          <h3 className="mb-0">Web Dev</h3>
+          <h4 className="">O'Clock Company</h4>
+          <p className="mb-2">
+          The goal of this training was to provide the necessary <strong className='text-emerald-500'>foundations</strong> for website development, teaching programming languages such as <strong className='text-emerald-500'>JavaScript</strong> or <strong className='text-emerald-500'>PHP</strong>.
+          </p>
+          <p className="mb-2">
+          A final project (<i><strong className='text-emerald-500'>Codit</strong>, see below</i>) was carried out over a duration of <strong className='text-emerald-500'>1 month</strong> to consolidate the knowledge acquired during the previous month.
+          </p>
+        </div>
+        <div className= "lg:w-2/3 lg:flex lg:justify-end">
+          <img src={computerlogo} alt="An illustration of a laptop used for programming. (Copyright @ https://fr.freepik.com/vecteurs-libre/illustration-concept-codage-main_21864184.htm#&position=0&from_view=user) " className="mt-4 md:mt-12 h-40 md:h-80 mx-auto lg:mx-0" />
+        </div>
       </>
     );
   }
@@ -108,7 +115,7 @@ function Experiences() {
           <span>2022 - 2023</span>
         </button>
       </div>
-      <section className="p-4 md:p-8  w-full text-center mt-4 flex flex-col">
+      <section className="p-4 md:p-8  w-full text-center mt-4 lg:mt-12 lg:w-4/5 lg:mx-auto lg:flex">
         {sectionContent}
       </section>
     </Section>
