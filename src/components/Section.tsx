9c375ecea3f2876ad56b59ce1a752010da164f62
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-04 18:11:55 +0200

    fix last typescript issue

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,53 +1,45 @@
-
 import { useContext } from "react";
 import { DarkModeContext } from "../contexts/DarkModeProvider";
 
-
 interface SectionProps {
-    id?: string; 
-    gradientToBl: boolean;
-    paddingBottom: boolean;
-    className?: string;
-    children: React.ReactNode;
+  id?: string;
+  gradientToBl: boolean;
+  paddingBottom: boolean;
+  className?: string;
+  children: React.ReactNode;
+}
+
+function Section({
+  id,
+  gradientToBl,
+  paddingBottom,
+  className,
+  children,
+}: SectionProps) {
+  const isGradientToBl = gradientToBl
+    ? "bg-gradient-to-bl from-slate-900 to-gray-700"
+    : "bg-gradient-to-tl from-slate-900 to-gray-700";
+
+  const isPaddingBottom = paddingBottom ? "pb-28 md:pb-32" : "";
+
+  const context = useContext(DarkModeContext);
+
+  if (!context) {
+    throw new Error("DarkModeContext is not available.");
   }
 
+  const { darkMode } = context;
 
-function Section ({
-    id,
-    gradientToBl,
-    paddingBottom,
-    className,
-    children,
-    }: SectionProps) {
+  const isDarkMode = darkMode ? isGradientToBl : "white-theme";
 
-    const isGradientToBl = gradientToBl
-    ? 'bg-gradient-to-bl from-slate-900 to-gray-700'
-    : 'bg-gradient-to-tl from-slate-900 to-gray-700';
-    
-    const isPaddingBottom = paddingBottom
-    ? 'pb-28 md:pb-32'
-    : '';
-
-    const context = useContext(DarkModeContext);
-
-    if (!context) {
-      throw new Error("DarkModeContext is not available.");
-    }
-  
-    const darkMode= context;
-
-    const isDarkMode = darkMode 
-
-    ? isGradientToBl
-    : 'white-theme';
-
-
-    return(
-        <section id= {`${id}`} className= {` ${isDarkMode}  ${isPaddingBottom} ${className} py-4 md:py-8 px-8 text-white`}>
-            <>{children}</>
-        </section>
-
-    )
+  return (
+    <section
+      id={`${id}`}
+      className={`${isDarkMode} ${isPaddingBottom} ${className} py-4 md:py-8 px-8 text-white`}
+    >
+      {children}
+    </section>
+  );
 }
 
 export default Section;
