80b2e511de192c3c213f8cee6c3de53f698dc1ea
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-04 18:46:14 +0200

    create save local storage of darkmode state

--- src/contexts/DarkModeProvider.tsx
+++ src/contexts/DarkModeProvider.tsx
@@ -1,5 +1,4 @@
-import { createContext, useState } from "react";
-
+import { createContext, useState, useEffect } from "react";
 
 interface DarkModeContextType {
   darkMode: boolean;
@@ -12,10 +11,15 @@ interface DarkModeProviderProps {
 
 export const DarkModeContext = createContext<DarkModeContextType | undefined>(undefined);
 
-export default function DarkModeProvider({
-  children,
-}: DarkModeProviderProps) {
-  const [darkMode, setDarkMode] = useState(true);
+export default function DarkModeProvider({ children }: DarkModeProviderProps) {
+  const [darkMode, setDarkMode] = useState(() => {
+    const storedDarkMode = localStorage.getItem("darkMode");
+    return storedDarkMode ? JSON.parse(storedDarkMode) : true;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
 
   function toggleDarkMode() {
     setDarkMode(!darkMode);
