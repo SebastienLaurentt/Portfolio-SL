73625a99f475b34f21ef21fe4108181ad4ef50f8
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-11 04:26:32 +0200

    change bg-cyan to bg-sky

--- src/components/ExperienceButton.tsx
+++ src/components/ExperienceButton.tsx
@@ -17,8 +17,8 @@ function ExperienceButton ( {activeButton, setActiveButton, experienceTitle, exp
         <button 
             className={`${
                 activeButton === experienceTitle
-                ? 'bg-cyan-500 text-white'
-                : 'bg-cyan-800 text-white md:hover:bg-cyan-500'
+                ? 'bg-sky-600 text-white'
+                : 'text-white bg-sky-900  md:hover:bg-sky-600 md:hover:text-white'
             } flex flex-col p-2 rounded-lg items-center`}
             onClick={() => handleButtonClick(experienceTitle)}
         >
