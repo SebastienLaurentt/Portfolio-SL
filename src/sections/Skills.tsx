01140b3e516d82343fe2b00e77f120234e74b815
SebastienLaurentt <slaurent.26@gmail.com>
2023-09-26 02:35:29 +0200

    optimize skills icon responsive

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -57,7 +57,7 @@ function Skills ({ gradientToBl }: { gradientToBl: boolean }) {
 
             {/* SKILLS LIST */}
             <ul 
-                className='text-center flex flex-wrap gap-y-8 justify-around mt-16 2xl:w-2/3 mx-auto'
+                className='text-center flex flex-wrap gap-8 justify-around mt-16 2xl:w-2/3 mx-auto'
             >
                 {data.map((value, id) => {
                     return (
@@ -72,10 +72,10 @@ function Skills ({ gradientToBl }: { gradientToBl: boolean }) {
                             }}   
                             custom={id}
                         >
-                            <svg viewBox="0 0 128 128" className='w-32 h-32 mb-2'>
+                            <svg viewBox="0 0 128 128" className='w-16 h-16 md:w-24 md:h-24 xl:w-32 xl:h-32 2xl:w-48 2xl:h-48 mb-2'>
                                 <g dangerouslySetInnerHTML={{ __html: value.data }} />
                             </svg>
-                            <span className='languageName'>{value.name}</span>
+                            <span>{value.name}</span>
                         </motion.li>
                     );
                 })}
